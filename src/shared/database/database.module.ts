d5e414c89c29d6aadcbba0e9835f9093f673d6e1
Kwabena Berko <kwabenaberko95@gmail.com>
2020-08-21 16:54:31 +0000

    Initial Commit

---
+++ src/shared/database/database.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { ObjectionModule } from "@willsoto/nestjs-objection"
+import * as knexfile from "./knexfile";
+
+@Module({
+    imports: [
+        ObjectionModule.register({
+            config: knexfile[process.env.NODE_ENV || "development"]
+        })
+    ]
+})
+export class DatabaseModule {}
