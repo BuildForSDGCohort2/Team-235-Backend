8ce3c9e8e9d749906814e47a3e7a7ecdbd36a06d
Kwabena Berko <kwabenaberko95@gmail.com>
2020-08-28 01:34:43 +0000

    Fixing Codacy Warnings

--- src/shared/database/migrations/20200827224226_create_users_table.ts
+++ src/shared/database/migrations/20200827224226_create_users_table.ts
@@ -5,7 +5,7 @@ import { User } from "../../../user/user.model";
 export async function up(knex: Knex): Promise<void> {
     const exists = await knex.schema.hasTable(User.tableName);
     if(!exists){
-        await knex.schema.raw(`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`);
+        await knex.schema.raw("CREATE EXTENSION IF NOT EXISTS 'uuid-ossp'");
         return await knex.schema.createTable(User.tableName, (table) => {
             table.uuid("id").primary().defaultTo(knex.raw("uuid_generate_v4()"));
             table.string("first_name").notNullable();
@@ -19,7 +19,7 @@ export async function up(knex: Knex): Promise<void> {
             table.uuid("updated_by_id").unsigned().references("id").inTable(User.tableName);
             table.timestamp("verified_at");
             table.timestamps(true, true);
-        })
+        });
     }
 }
 
