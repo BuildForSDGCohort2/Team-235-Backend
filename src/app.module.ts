209be9c37b5430755d5877fd296dff941216347f
Botwe Asamoah Emmanuel <noelnuel44@gmail.com>
2020-09-08 10:07:25 +0000

    fixed codacy code check issue

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,7 +3,7 @@ import { GraphQLModule } from "@nestjs/graphql";
 import { DatabaseModule } from "./shared/database/database.module";
 import { AuthenticationModule } from "./authentication/authentication.module";
 import { UserModule } from "./user/user.module";
-import { CategoryModule } from './category/category.module';
+import { CategoryModule } from "./category/category.module";
 
 @Module({
   imports: [DatabaseModule,
