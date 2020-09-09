98e7321215e198a6dfbe3d43081988c272c8bbf1
Botwe Asamoah Emmanuel <noelnuel44@gmail.com>
2020-09-09 06:50:42 +0000

    added migration

---
+++ src/shared/database/migrations/20200909063211_create_categories_table.ts
@@ -0,0 +1,23 @@
+import * as Knex from "knex";
+import { Category } from "../../../category/category.model";
+
+import { User } from "../../../user/user.model";
+
+export async function up(knex: Knex): Promise<void> {
+    const exists = await knex.schema.hasTable(Category.tableName);
+    if (!exists) {
+        await knex.schema.raw("CREATE EXTENSION IF NOT EXISTS \"uuid-ossp\"");
+        return await knex.schema.createTable(Category.tableName, (table) => {
+            table.uuid("id").primary().defaultTo(knex.raw("uuid_generate_v4()"));
+            table.string("name").notNullable();
+            table.uuid("created_by_id").unsigned().references("id").inTable(User.tableName);
+            table.uuid("updated_by_id").unsigned().references("id").inTable(User.tableName);
+            table.timestamps(true, true);
+        });
+    }
+}
+
+
+export async function down(knex: Knex): Promise<void> {
+}
+
