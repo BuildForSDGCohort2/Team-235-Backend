7594762f478354bc54dbb2d94dcf09b2e5012ff4
Kwabena Berko <kwabenaberko95@gmail.com>
2020-10-11 06:41:08 +0000

    Fixing Heroku Issue

--- src/main.ts
+++ src/main.ts
@@ -9,6 +9,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   await DataInitializer.init();
-  await app.listen(process.env.PORT);
+  await app.listen(process.env.PORT, process.env.HOST);
 }
 bootstrap();
